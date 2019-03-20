ac780cda31d3d0c9c3c2c91e3b59743255f296c6
Vidumini <heartyvds@gmail.com>
2019-03-20 15:19:29 +0530

    added UI service

--- Minimus/src/app/ui-service.service.ts
+++ Minimus/src/app/ui-service.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class UiServiceService {
-
-  constructor() { }
+@Injectable()
+export class UiServiceService
+{
+  darkModeState: BehaviorSubject<boolean>;
+  constructor()
+  {
+    this.darkModeState = new BehaviorSubject<boolean>(false);
+  }
 }
