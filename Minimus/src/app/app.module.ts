9b5cfe9eae6e35f2bd7b8cb6f9fb413d47c95ca5
Vidumini <heartyvds@gmail.com>
2019-03-20 16:21:36 +0530

    updated app module

--- Minimus/src/app/app.module.ts
+++ Minimus/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddCardComponent } from './add-card/add-card.component';
+import { AddComponent } from './add/add.component';
 import { DetailsComponent } from './details/details.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { UiServiceService } from './ui-service.service';
+import { WeatherService } from './weather.service';
 
 @NgModule({
   declarations: [
@@ -16,15 +23,21 @@ import { SignupComponent } from './signup/signup.component';
     HomeComponent,
     WeatherCardComponent,
     AddCardComponent,
+    AddComponent,
     DetailsComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule
   ],
-  providers: [],
+  providers: [
+    WeatherService,
+    UiServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
