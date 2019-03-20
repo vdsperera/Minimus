ce9c0c706261c3247f19f2428072c80b633776b2
Vidumini <heartyvds@gmail.com>
2019-03-20 16:20:41 +0530

    updated app component

--- Minimus/src/app/app.component.ts
+++ Minimus/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiServiceService } from './ui-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Minimus';
+export class AppComponent implements OnInit
+{
+  showMenu = false;
+  darkModeActive: boolean;
+
+  constructor(public ui: UiServiceService)
+  {
+
+  }
+
+  ngOnInit()
+  {
+    this.ui.darkModeState.subscribe((value) => {
+      this.darkModeActive = value;
+    });
+  }
+
+  toggleMenu()
+  {
+    this.showMenu = !this.showMenu;
+  }
+
+  modeToggleSwitch()
+  {
+    this.ui.darkModeState.next(!this.darkModeActive);
+  }
+
 }
